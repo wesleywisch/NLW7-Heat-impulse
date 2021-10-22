0d117561ccced06d3790c8e04ff2e18099e0764e
wesley_wisch <wesley_wisch@hotmail.com>
2021-10-22 08:36:22 -0300

    Header e o componente de foto de usuário pronto

---
+++ mobile/src/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+import { Header } from '../../components/Header';
+
+export function Home() {
+  return(
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+};
